96a692349c3687aa39765d22148e0c9920560ecb
Bakyt Naurzalinov <bakyt92@mail.ru>
2026-03-29 19:15:04 +0200

    updates to input

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 import sys
+from ft_est_price.ft_estimate import ft_estimate
+from ft_train.ft_train import ft_train
 
 def main():
 	try:
 		question1 = input("Would you like Estimate Price (write EP) or Train Model (TM)? ")
 		if question1 == "EP":
 			est_res = ft_estimate()
-		else if question1 == "TM":
+		elif question1 == "TM":
 			train_res = ft_train()
+		else:
+			raise ValueError("wrong input")
 	except Exception as e:
 		print("Error {e}")
 		sys.exit(1)
