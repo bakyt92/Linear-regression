9ba20f29752fb0442566d6bd25115d03c218e7ad
bakyt92 <bakyt92@mail.ru>
2026-04-01 23:32:43 +0400

    updates - processing input data

--- ft_utils/utils.py
+++ ft_utils/utils.py
@@ -8,12 +8,28 @@ class Reader_files:
 		try:
 			with open(link_file, mode="r", encoding="utf-8") as file:
 				content = file.read()
-				self.data = content
-			# raw_data = Process_Data(content)
+			raw_data = Process_Data(content)
 		except Exception as e:
 			print(f"Exception: {e}")
+			sys.exit(1)
 		return
 
-    # def Process_Data(self, content):
-		
-    #     return
+	def Process_Data(self, content):
+		try:
+			header = []
+			raw_data = []
+			for x in content[0]:
+				if isinstance(x, str):
+					header.append(x)
+			for rows in content:
+				if all(isinstance(x, (int, float)) for x in rows):
+					raw_data.append(rows)
+				else: 
+					continue
+
+
+
+		except Exception as e:
+			print(f"Exception: {e}")
+			sys.exit(1)
+		return
--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 import sys
-from ft_est_price.ft_estimate import ft_estimate
-from ft_train.ft_train import ft_train
+# from ft_est_price.ft_estimate import ft_estimate
+# from ft_train.ft_train import ft_train
 from ft_utils.utils import Reader_files
 
 
 def main():
+	Reader = Reader_files()
+	Reader.Read_file(sys.argv[1])
+	print("PRINT CONTENTS OF SYS.ARGV: Res.data")
+	print(Reader.data)
 	# try:
 	# 	question1 = input("Would you like Estimate Price (write EP) or Train Model (TM)? ")
 	# 	if question1 == "EP":
@@ -16,10 +20,7 @@ def main():
 	# except Exception as e:
 	# 	print("Error {e}")
 	# 	sys.exit(1)
-	Res = Reader_files()
-	Res.Read_file(sys.argv[1])
-	print("PRINT CONTENTS OF SYS.ARGV: Res.data")
-	print(Res.data)
+	
 	return
 
 
